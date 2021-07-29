eba80fef38cbc397d12d6054d79d0e2dc411b9b9
unknown <lucrezia.lyyneheym@gmail.com>
2021-07-29 17:38:40 +0700

    user

--- Controllers/controller.js
+++ Controllers/controller.js
@@ -1,7 +1,9 @@
-const {HastagCat, Post, PostHastag , Profile , UserCat} = require("../models")
+const {HastagCat, Post, PostHastag , Profile , UserCat, Register} = require("../models")
+
+
 class Controller{
     static home(req,res){
-        res.send(masuk)
+        res.send("masuk")
     }
 }
 module.exports = Controller
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,26 @@
 const router = require("express").Router()
 const Controller= require("../Controllers/controller")
+const userController = require("../Controllers/userController")
 
 router.get('/beranda', Controller.home)
 router.get('/bikinPost', Controller.home)
 router.get('/deletepost/:id', Controller.home)
 router.get('/editpost/:id', Controller.home)
 router.get('/editProfile/:id', Controller.home)
+
+router.get("/register", userController.registerForm)
+router.post("/register", userController.registerData)
+
+router.get("/login", userController.formLogin)
+router.post("/login", userController.login)
+
+function checkisLogin(req, res, next) {
+    if (req.session.isLogin) {
+        next()
+    } else {
+        res.redirect("/")
+    }
+}
 
 
 module.exports = router
